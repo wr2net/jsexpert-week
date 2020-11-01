b79dc803b0ab67ab0595a0d73b899390bfb3d06c
Wagner <wagner@rigolisarosa.com>
2020-11-01 17:22:21 -0300

    final implementation

--- public/bandersnatch/src/videoPlayer.js
+++ public/bandersnatch/src/videoPlayer.js
@@ -6,6 +6,7 @@ class VideoMediaPlayer {
 
         this.videoElement = null
         this.sourceBuffer = null
+        this.activeItem = {}
         this.selected = {}
         this.videoDuration = 0
         this.selections = []
@@ -28,7 +29,7 @@ class VideoMediaPlayer {
         const mediaSource = new MediaSource()
         this.videoElement.src = URL.createObjectURL(mediaSource)
 
-        mediaSource.addWventListener("sourceopen", this.sourceOpenWrapper(mediaSource))
+        mediaSource.addEventListener("sourceopen", this.sourceOpenWrapper(mediaSource))
     }
 
     sourceOpenWrapper(mediaSource) {
@@ -39,20 +40,60 @@ class VideoMediaPlayer {
             // evita rodar como "LIVE"
             mediaSource.duration = this.videoDuration
             await this.fileDownload(selected.url)
-            this.waitForQuestions()
             setInterval(this.waitForQuestions.bind(this), 200)
         }
     }
 
     waitForQuestions() {
-        const currenTime = parseInt(this.videoElement.currentTime)
+        const currentTime = parseInt(this.videoElement.currentTime)
+        const option = this.selected.at === currentTime
+        if(!option) return;
+
+        // evita que o modal seja aberto 2x no mesmo segundo
+        if(this.activeItem.url === this.selected.url) return;
         this.videoComponent.configureModal(this.selected.options)
+        this.activeItem = this.selected
+    }
+
+    async currentFileResolution() {
+        const LOWEST_RESOLUTION = 144
+        const prepareUrl = {
+            url: this.manifestJSON.finalizar.url,
+            fileResolution: LOWEST_RESOLUTION,
+            fileResolutionTag: this.manifestJSON.fileResolutionTag,
+            hostTag: this.manifestJSON.hostTag
+        }
+        const url = this.network.parseManifestURL(prepareUrl)
+        return this.network.getProperResolution(url)
+    }
+
+    async nextChunk(data) {
+        const key = data.toLowerCase()
+        const selected = this.manifestJSON[key]
+        this.selected = {
+            ...selected,
+            // ajusta o tempo que o modal vai aparecer, baseado no tempo corrente
+            at: parseInt(this.videoElement.currentTime + selected.at)
+        }
+        this.manageLag(this.selected)
+        // deixe o restante do video rodar enquanto baixa o novo video
+        this.videoElement.play()
+        await this.fileDownload(selected.url)
+    }
+
+    manageLag(selected) {
+        if(!!~this.selections.indexOf(selected.url)) {
+            selected.at += 5
+            return;
+        }
+        this.selections.push(selected.url)
     }
 
     async fileDownload(url) {
+        const fileResolution = await this.currentFileResolution()
         const prepareUrl = {
             url,
-            fileResolution: 360,
+            fileResolution,
             fileResolutionTag: this.manifestJSON.fileResolutionTag,
             hostTag: this.manifestJSON.hostTag
         }
@@ -62,10 +103,10 @@ class VideoMediaPlayer {
         return this.processBufferSegments(data)
     }
 
-    setVideoPlayerDuration() {
-        const bars = finalUrl.split('/')
-        const [ name, videoDuration] = bars[bars.length - 1].split('-')
-        this.videoDuration += videoDuration
+    setVideoPlayerDuration(finalURL) {
+        const bars = finalURL.split('/')
+        const [name, videoDuration] = bars[bars.length - 1].split('-')
+        this.videoDuration += parseFloat(videoDuration)
     }
 
     async processBufferSegments(allSegments) {
@@ -73,14 +114,14 @@ class VideoMediaPlayer {
         sourceBuffer.appendBuffer(allSegments)
 
         return new Promise((resolve, reject) => {
-            const updateEnd = () => {
+            const updateEnd = (_) => {
                 sourceBuffer.removeEventListener("updateend", updateEnd)
                 sourceBuffer.timestampOffset = this.videoDuration
 
                 return resolve()
             }
 
-            sourceBuffer.addEventListener("updateend", () => {})
+            sourceBuffer.addEventListener("updateend", updateEnd)
             sourceBuffer.addEventListener("error", reject)
         })
     }
